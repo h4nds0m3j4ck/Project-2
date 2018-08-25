a11d3d5c5e07ed31260d02302d6287302a48e60f
CardBoardHero <yuniorgarciga@gmail.com>
2018-08-25 11:36:22 -0400

    routes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -11,6 +11,26 @@ module.exports = function(app) {
     });
   });
 
+  // owners page
+  app.get("/owner", function(req, res) {
+    db.Example.findAll({}).then(function(dbExamples) {
+      res.render("owner", {
+        msg: "Welcome!",
+        examples: dbExamples
+      });
+    });
+  });
+
+  // find page
+  app.get("/find", function(req, res) {
+    db.Example.findAll({}).then(function(dbExamples) {
+      res.render("owner", {
+        msg: "Welcome!",
+        examples: dbExamples
+      });
+    });
+  });
+
   // Load example page and pass in an example by id
   app.get("/example/:id", function(req, res) {
     db.Example.findOne({ where: { id: req.params.id } }).then(function(
