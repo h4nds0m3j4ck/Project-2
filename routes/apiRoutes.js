3b6ef66f6a4f7f9cc48f7e84facb965f5f3ef4f3
CardBoardHero <yuniorgarciga@gmail.com>
2018-08-27 19:14:20 -0400

    fix

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -10,16 +10,14 @@ module.exports = function(app) {
 
   // Create a new owner
   app.post("/api/owners", function(req, res) {
-    db.Owner.create(req.body).then(function(dbOwner) {
+    db.Owner.create(req.body).then(function(dbOwners) {
       res.json(dbOwners);
     });
   });
 
   // Delete an owner by id
   app.delete("/api/owners/:id", function(req, res) {
-    db.Owner.destroy({ where: { id: req.params.id } }).then(function(
-      dbOwner
-    ) {
+    db.Owner.destroy({ where: { id: req.params.id } }).then(function(dbOwners) {
       res.json(dbOwners);
     });
   });
